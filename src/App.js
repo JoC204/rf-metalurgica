d6efcf2d772e7525e982b5337c54e974653f9bac
JoC204 <jbarrionuevo204@gmail.com>
2025-01-11 11:52:40 -0300

    Agrego estilos globales y adopto header para los diferentes mobiles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./Styles/Global.css";
 import Header from "./components/Header";
 import Home from "./components/Home"; // Importamos el nuevo componente
 import About from "./components/About"; // Importamos el nuevo componente
