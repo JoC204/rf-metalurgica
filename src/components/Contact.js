88b75b65c035533605f2a1caaec84fa58d6711cb
JoC204 <jbarrionuevo204@gmail.com>
2025-03-27 20:02:53 -0300

    Mejoro componente Contact.js y su estilo Contact.css
    Falta terminar imagen de portada

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "../Styles/Contact.css";
 import {
-  FaPhone,
   FaEnvelope,
   FaClock,
   FaMapMarkerAlt,
-  FaFacebookF,
   FaInstagram,
   FaWhatsapp,
   FaLinkedinIn,
   FaYoutube,
+  FaWhatsappSquare,
 } from "react-icons/fa";
 
 const Contact = () => {
@@ -18,27 +17,63 @@ const Contact = () => {
       <div className="contact-image"></div>
 
       <div className="contact-content">
-        <h2>Teléfonos</h2>
         <div className="contact-info">
-          <p><FaPhone className="icon" /> +341-156 579576</p>
-          <p><FaPhone className="icon" /> +341-156 579576</p>
+          <div className="contact-info-item">
+            <div className="contact-info-item-icon">
+              <p>
+                <FaWhatsappSquare className="icon" />
+              </p>
+            </div>
+            <div className="contact-info-text">
+              <h3>Teléfonos</h3>
+              <p>+341-156 579576</p>
+            </div>
+          </div>
 
-          <h3>Correo electrónico</h3>
-          <p><FaEnvelope className="icon" />riccifabricio@yahoo.com.ar</p>
+          <div className="contact-info-item">
+            <div className="contact-info-item-icon">
+              <p>
+                <FaEnvelope className="icon" />
+              </p>
+            </div>
+            <div className="contact-info-text">
+              <h3>Correo electrónico</h3>
+              <p>riccifabricio@yahoo.com.ar</p>
+            </div>
+          </div>
 
-          <h3>Horarios de atención</h3>
-          <p><FaClock className="icon" /> Lunes a Viernes, 8:00 - 17:00</p>
+          <div className="contact-info-item">
+            <div className="contact-info-item-icon">
+              <p>
+                <FaClock className="icon" />
+              </p>
+            </div>
+            <div className="contact-info-text">
+              <h3>Horarios de atención</h3>
+              <p>Lunes a Viernes, 8:00 - 17:00</p>
+            </div>
+          </div>
 
-          <h3>Ubicación</h3>
-          <p><FaMapMarkerAlt className="icon" /> Av. Ovidio Lagos 6335, S2000 Rosario, Santa Fe</p>
+          <div className="contact-info-item">
+            <div className="contact-info-item-icon">
+              <p>
+                <FaMapMarkerAlt className="icon" />
+              </p>
+            </div>
+            <div className="contact-info-text">
+              <h3>Ubicación</h3>
+              <p>Av. Ovidio Lagos 6335, S2000 Rosario, Santa Fe</p>
+            </div>
+          </div>
 
+          <div className="redes">
           <h3>Nuestras redes</h3>
           <div className="social-icons">
-            <FaFacebookF />
             <FaInstagram />
             <FaWhatsapp />
             <FaLinkedinIn />
             <FaYoutube />
+          </div>
           </div>
         </div>
       </div>
