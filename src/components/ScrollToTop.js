e93d5f882c1114a1d1cdd8686aed10d872e24a19
JoC204 <jbarrionuevo204@gmail.com>
2025-03-29 11:36:16 -0300

    Mejoro el top en ScrollToTop.js par que llegue al top verdadero
    Controlar

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -6,7 +6,7 @@ const ScrollToTop = () => {
 
   useEffect(() => {
     window.scrollTo({
-      top: 0,
+      top: -80,
       behavior: "smooth", // o "auto" si no querés animación
     });
   }, [pathname]);
